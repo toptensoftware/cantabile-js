3a13d0699d300484750bbdacab055e0fa74e2ec9
Brad Robinson <contact@toptensoftware.com>
2026-03-18 10:42:30 +1100

    Fix docs for new methods

--- SetList.js
+++ SetList.js
@@ -147,6 +147,7 @@ class SetList extends EndPoint
 
 	/**
 	 * Gets a list of available set lists in the user's set list folder
+	 * @method available
 	 * @returns {String[]} An array of set list names (relative to user's set list folder, extension removed)
 	 */
 	async available()
@@ -156,6 +157,7 @@ class SetList extends EndPoint
 
 	/**
 	 * Loads the specified set list from the user's set list folder
+	 * @method loadSetList
 	 * @param {String} name Name of the set to load (relative to user's set list folder, without extension)
 	 * @param {Boolean} loadFirst True to load the first song in the set list (default = true)
 	 */
